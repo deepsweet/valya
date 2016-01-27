81a932394de7c627f7c0f298a5c2e3f428896de3
mistadikay <koltsov.d@gmail.com>
2016-01-27 15:41:46 +0700

    :space_invader: fix tests not running due to incorrect babel config

--- test/validator.js
+++ test/validator.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import Valya from '../lib/';
 
-@Valya
 class Validator extends React.Component {
     render() {
         return <div>{this.props.children}</div>;
     }
 }
 
-export default Validator;
+export default Valya(Validator);
