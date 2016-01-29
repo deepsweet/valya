64036caaaefc0d96ad11ed53a3375d0ad8fb09d8
mistadikay <koltsov.d@gmail.com>
2016-01-29 11:08:20 +0700

    :recycle: upgrade to start@2

--- tasks/index.js
+++ tasks/index.js
@@ -1,6 +1,24 @@
+import Start from 'start';
+import logger from 'start-simple-logger';
 import clean from 'start-clean';
 import eslint from 'start-eslint';
 
-export const lint = eslint();
-export const cleanBuild = clean('build/');
-export const cleanCoverage = clean('coverage/');
+const start = Start(logger);
+
+export function lint() {
+    return start(
+        eslint()
+    );
+}
+
+export function cleanBuild() {
+    return start(
+        clean('build/')
+    );
+}
+
+export function cleanCoverage() {
+    return start(
+        clean('coverage/')
+    );
+}
